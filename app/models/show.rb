25208335699ff85e96ee15a35661e7e611573775
Timothy Ege <retibra@gmail.com>
2018-07-18 12:45:30 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,14 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
-  def most_popular_show
-    Show.maximum(:rating)
+  def self.most_popular_show
+    Show.highest_rating
   end
 
+  def self.least_popular_show
+    Show.minimum(:rating)
+  end
 end
